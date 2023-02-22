bf76f9e4aa9813af89d959e460a00c1c79f89e3b
Julian Ng <julesnuggy@gmail.com>
2023-02-22 12:13:03 +0000

    Add Standard Criteria tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -4,14 +4,42 @@ describe('Todo List', () => {
     cy.request(Cypress.env("apiUrl")).as("todos");
   });
 
-  it('shows the Todo List', () => {
-    cy.get("@todos").should((response) => {
-      expect(response.body).to.have.length(2)
-    })
+  describe('Standard Criteria', () => {
+    it('Loads and renders the Todo List from the server', () => {
+      cy.get("@todos").should((response) => {
+        expect(response.body).to.have.length(3)
+      })
 
-    cy.get(".todo-list > li")
-      .should("have.length", 2)
-      .eq(0)
-      .should("contain", "Feed the cat");
+      cy.get(".todo-list > li").should("have.length", 3)
+
+      cy.get(".todo-list > li")
+        .eq(0)
+        .should("contain", "Feed the cat")
+
+      cy.get(".todo-list > li")
+        .should("have.length", 3)
+        .should("contain", "Cut the grass")
+
+      cy.get(".todo-list > li")
+        .eq(2)
+        .should("contain", "Fix the gutters")
+    });
+
+    it('Shows completed items with grey font and is crossed-out',  () => {
+      cy.get(".todo-list > li")
+        .eq(0)
+        .should('have.css', 'color', 'rgb(0, 0, 0)')
+        .and('not.have.css', 'text-decoration', 'line-through');
+
+      cy.get(".todo-list > li")
+        .eq(1)
+        .should('have.css', 'color', 'rgb(128, 128, 128)')
+        .and('have.css', 'text-decoration', 'line-through solid rgb(128, 128, 128)');
+
+      cy.get(".todo-list > li")
+        .eq(2)
+        .should('have.css', 'color', 'rgb(128, 128, 128)')
+        .and('have.css', 'text-decoration', 'line-through solid rgb(128, 128, 128)');
+    });
   });
-})
+});
