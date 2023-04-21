c34821d6cf7ecd65a7022a70a883ffda3847f296
tommyk257 <tommyk257@gmail.com>
2023-04-21 17:41:45 +0100

    submit working

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function toDoList() {
 toDoList();
 
 function displayActivities() {
+  ul.innerHTML = ''
   state.todos.forEach((toDoItems) => {
     const list = document.createElement("li");
 
@@ -30,4 +31,38 @@ function displayActivities() {
 
 // POST request
 
+function createNewTodo(newTask){
+  
+  const newAct = {
+    title: newTask  
+  }
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newAct)
+  }
+   fetch("http://localhost:3000/todos", options)
+  .then(function (response) {
+   return response.json()
+  })
+  .then(function(data) {
+    state.todos.push(data)   
+    displayActivities();         
+  })
+}
+
+
+const form  = document.querySelector("form")
+
+form.addEventListener("submit" ,(event) => {
+event.preventDefault()
+
+const input = document.querySelector("input")
+
+const newTask = input.value 
+createNewTodo(newTask)
+form.reset()
+})
 
