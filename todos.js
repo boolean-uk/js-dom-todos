aeeac7815601e05e911520d74653b1ba640d4488
carolarruda <carolinacalouroarruda@gmail.com>
2023-04-21 17:26:27 +0200

    finished core

---
+++ todos.js
@@ -0,0 +1,82 @@
+const toDoList = document.querySelector('ul')
+const form = document.querySelector('form')
+const titleInput = document.querySelector('input')
+
+const state = {
+    todos: []
+}
+
+// fetch the json
+
+function getTodoList() {
+
+    fetch("http://localhost:3000/todos")
+        
+    .then(function (response) {
+        console.log('response returned..', response)
+        console.log('response body', response.body)
+        return response.json()
+    })  
+        .then(function (data) {
+            console.log('To do List', data);
+            state.todos = data
+            renderTodoList()
+        })
+    console.log('End of getTodoList');
+}
+
+// render code
+
+function renderTodoList() {
+    toDoList.innerHTML = ''
+    state.todos.forEach(todo => { if (todo.title !== undefined) {
+        const li = document.createElement('li');
+        li.innerText = `${todo.title}`
+        if (todo.completed === true) {
+            li.setAttribute('class', 'completed')
+        }
+        toDoList.append(li)
+    }
+})
+
+}
+
+
+function createNewTodo(newTodoitem) {
+    const newTodo = {
+    title: newTodoitem
+    }
+    
+    const options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newTodo)
+    }
+
+    fetch('http://localhost:3000/todos', options)
+        .then(function (response) {
+        return response.json()
+        })
+        .then(function (data) {
+            state.todos.push(data)
+            renderTodoList()
+    })
+
+}
+
+// EVENT HANDLES
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const newTodoitem = titleInput.value
+    createNewTodo(newTodoitem);
+
+    form.reset()
+})
+
+
+getTodoList()
+renderTodoList()
