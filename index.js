cc2b2b6c9a7edcf5016b8533b56abb4a37737308
Alex Shaw <alexjshaw@gmail.com>
2023-04-21 14:01:14 +0100

    Pulls data from todos.json and displays on page

---
+++ index.js
@@ -0,0 +1,36 @@
+const state = {
+    toDos: []
+}
+
+const toDoList = document.querySelector(`#todo-list`)
+const newForm = document.querySelector(`form`)
+
+// Pull todos from server
+// Render todos on page
+
+function getToDos() {
+    // console.log(`getToDos is running`)
+
+    fetch(`http://localhost:3000/todos`)
+    .then(function (response) {
+        // console.log(`response...`, response)
+        return response.json()
+    })
+    .then(function (data) {
+        // console.log(`data...`, data)
+        state.toDos = data
+        renderToDos()
+    })
+}
+
+function renderToDos() {
+    toDoList.innerHTML = ``
+    // console.log(`state`, state.toDos)
+    state.toDos.forEach(toDo => {
+        const li = document.createElement(`li`)
+        li.innerText = toDo.title
+        toDoList.append(li)
+    })
+}
+
+getToDos()
