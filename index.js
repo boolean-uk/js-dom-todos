40f46defffc7926a65968b4030d69bec50f64d47
NoroAxper <norikvardanyan.w@gmail.com>
2023-04-22 19:47:04 +0100

    little update

--- index.js
+++ index.js
@@ -44,16 +44,52 @@ const postTodos = (newTodo) => {
     renderTodos()
     })
 }
+const updateTodo = (todo) => {
+        const updatedTodo = {
+        completed: true
+        }
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(updatedTodo)
+    }
+    console.log(todo.id)
+    fetch(`http://localhost:4000/contacts/${todo.id}`, options)
+    .then(function (response) {
+      return response.json()
+    })
+    .then((data) => {
+        if (todo.completed === true) {
+            return todo.completed = false
+        }
+        if (todo.completed === false) {
+            return todo.completed = true
+        }
+    })
+    renderTodos()
+  }
+  // really ill with 39 degrees so i don't have energy for this anymore, sorry :(
 const renderTodos = () => {
     todoUl.innerHTML = ""
     state.todos.forEach(todo => {
         const li = document.createElement('li')
+        const button = document.createElement('button')
+        const delButton = document.createElement('button')
+        delButton.innerText = 'Delete'
         li.innerText = `${todo.title}`
+        button.innerText = 'Uncomplete'
         if (todo.completed === true) {
             li.style.textDecoration = 'line-through'
             li.style.color = 'grey'
+            button.innerText = 'Completed'
         }
+        button.addEventListener('click', () => {
+            updateTodo(todo)
+        })
         todoUl.append(li)
+        li.append(button, delButton)
     })
 }
 getTodos()
