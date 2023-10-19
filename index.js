b27b3884f1967e4fac468d628764f9ab2588ac9b
Jack Miller <jdmiller1991@outlook.com>
2023-10-19 17:00:53 +0100

    Latest

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
-// State for storing all the information
+// State for storing all the information.
 const state = {
     todos: []
 }
-
-
+// Linking page to JS.
 const toDoList = document.querySelector('#todo-list')
 const form = document.querySelector('form')
 const root = "http://localhost:3000/todos"
 
-const renderToDoList
+// Rendering the Todo list.
+function renderToDoList () {
+    toDoList.innerHTML = ''
+    state.todos.forEach((toDoThings) => {
+        const listItems = document.createElement('li')
+        listItems.innerText = toDoThings.title
+        toDoList.append(listItems)
+    })
+}
 
 
+function addToDo (newToDo) {
+    const newtodo = {
+        title: newItem,
+        completed: false
+    }
 
 
-const addToDo
+}
 
-
+form.addEventListener('submit', (event) => {
+    const newToDo = input.value
+    addToDo(newToDo)
+    form.reset()
+})
 
 
 
