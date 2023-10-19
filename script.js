593cf1c01d4fdcf90f284980d74caf63b31ada1a
TomEastwood <t.eastwood99@gmail.com>
2023-10-19 18:25:24 +0100

    added PATCH functionality with an argument to change the completed button innerText on completed tasks, and separate function to remove buttons from completed tasks alltogether

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const state = {
-    task : []
+    task: []
 }
 
 const root = 'http://localhost:3001'
@@ -9,13 +9,15 @@ const taskList = document.querySelector('#todo-list')
 //GET function to render all tasks
 const getAndRender = () => {
     fetch(`${root}/todos`)
-    .then((response) => response.json())
-    .then(data => {
-        console.log("render all tasks", data)
-        state.task = data
-        removeTasks()
-        renderTasks()
-    })
+        .then((response) => response.json())
+        .then(data => {
+            console.log("render all tasks", data)
+            state.task = data
+            removeTasks()
+            renderTasks()
+            addCompleteButton()
+            // removeCompleteButton()
+        })
 }
 
 //POST function to add a new task
@@ -31,15 +33,15 @@ const createTask = (event) => {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(data) 
+        body: JSON.stringify(data)
     }
 
     fetch(`${root}/todos`, options)
-    .then((response) => response.json())
-    .then(data => {
-        console.log("create new task", data)
-        getAndRender()
-    })
+        .then((response) => response.json())
+        .then(data => {
+            console.log("create new task", data)
+            getAndRender()
+        })
 }
 
 const renderTasks = () => {
@@ -49,9 +51,57 @@ const renderTasks = () => {
             taskItem.classList.add('completed')
         }
         taskItem.innerText = task.title
+        const completeButton = addCompleteButton(task)
+        taskItem.append(completeButton)
         taskList.appendChild(taskItem)
     })
 }
+
+//function that adds a complete button to each uncompleted task in the list 
+const addCompleteButton = (task) => {
+    const completeButton = document.createElement('button')
+    completeButton.innerText = 'Complete'
+    //argument to change innerText on completed task buttons
+    if (task.completed) {
+        completeButton.innerText = 'Revisit'
+    }
+    //event listener to call my completeTask function that changes the completed status of the task to true
+    completeButton.addEventListener('click', (event) => {
+        completeTask(task.id, !task.completed)
+
+    })
+    return completeButton
+}
+
+//function that changes the completed status of the task to true
+const completeTask = (id, completed) => {
+    const data = {
+        completed: completed
+    }
+
+    const options = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    fetch(`${root}/todos/${id}`, options)
+        .then((response) => response.json())
+        .then(data => {
+            console.log("complete task", data)
+            getAndRender()
+        })
+}
+
+//alternate function that removes the completed button from completed tasks
+// const removeCompleteButton = () => {
+//     const completedTasks = document.querySelectorAll('.completed')
+//     completedTasks.forEach(task => {
+//         task.querySelector('button').remove()
+//     })
+// }
 
 taskForm.addEventListener('submit', (event) => {
     event.preventDefault()
