a53dccb948ea0bbcb5afedabe58e38fe9aa44587
TomEastwood <t.eastwood99@gmail.com>
2023-10-19 15:50:53 +0100

    POST functionality added to create new task and append to list

--- script.js
+++ script.js
@@ -6,13 +6,39 @@ const root = 'http://localhost:3001'
 const taskForm = document.querySelector('form')
 const taskList = document.querySelector('#todo-list')
 
+//GET function to render all tasks
 const getAndRender = () => {
     fetch(`${root}/todos`)
     .then((response) => response.json())
     .then(data => {
         console.log("render all tasks", data)
         state.task = data
+        removeTasks()
         renderTasks()
+    })
+}
+
+//POST function to add a new task
+const createTask = (event) => {
+    const data = {
+        id: event.target.id.value,
+        title: event.target.title.value,
+        completed: false
+    }
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) 
+    }
+
+    fetch(`${root}/todos`, options)
+    .then((response) => response.json())
+    .then(data => {
+        console.log("create new task", data)
+        getAndRender()
     })
 }
 
@@ -25,6 +51,15 @@ const renderTasks = () => {
         taskItem.innerText = task.title
         taskList.appendChild(taskItem)
     })
+}
+
+taskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    createTask(event)
+})
+
+const removeTasks = () => {
+    taskList.innerHTML = ''
 }
 
 getAndRender()
