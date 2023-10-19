7ff7f096e26d1877ac1b6accc4f602c532ff3668
Pierluigi <pierluigi.capirci89@gmail.com>
2023-10-19 19:02:33 +0100

    core done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const root = 'http://localhost:3000';
 const newTodosList = document.querySelector('#todo-list');
-const addNewTodos = document.querySelector('input');
+const addNewTodos = document.querySelector('form');
+
+const state = {
+    todos: [],
+};
 
 function listAllTodos() {
 const options = {
@@ -12,17 +16,48 @@ const options = {
 
 fetch(`${root}/todos`, options)
   .then(response => response.json())
-  .then(jsonData => {
-    jsonData.forEach(todo => {
+  .then(jsonData => {console.log(jsonData);
+    state.todos = jsonData;
+    renderTodos();
+  });
+}
+
+function renderTodos() {
+    newTodosList.innerHTML = '';
+    state.todos.forEach(todo => {
         const li = document.createElement('li');
         if (todo.completed) {
             li.classList.add('completed');
-            li.innerText = todo.title;
+            li.innerHTML = todo.title;
             newTodosList.appendChild(li);
         } else {
-            li.innerText = todo.title;
+            li.innerHTML = todo.title;
             newTodosList.appendChild(li);
-        }});
+        }
+    });
+}
+
+addNewTodos.addEventListener('submit', (element) => {
+    element.preventDefault();
+    const data ={
+        title: element.target.title.value,
+        completed: false,
+    }
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    }
+    fetch(`${root}/todos`, options)
+    .then(response => response.json())
+    .then(jsonData => {state.todos.push(jsonData)})
+    element.target[0].value = '';
+    listAllTodos();
 });
-};
+        
 listAllTodos();
+
+
+
