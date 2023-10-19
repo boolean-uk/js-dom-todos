5ca460892488c7e802a848178170f3dddec7dea5
Jack Miller <jdmiller1991@outlook.com>
2023-10-19 16:35:47 +0100

    First update

---
+++ index.js
@@ -0,0 +1,23 @@
+// State for storing all the information
+const state = {
+    todos: []
+}
+
+
+const toDoList = document.querySelector('#todo-list')
+const form = document.querySelector('form')
+const root = "http://localhost:3000/todos"
+
+const renderToDoList
+
+
+
+
+const addToDo
+
+
+
+
+
+
+
