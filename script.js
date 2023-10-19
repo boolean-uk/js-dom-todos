b60dc23d8372e5fc0a00115f0b4ee54a886037cc
TomEastwood <t.eastwood99@gmail.com>
2023-10-19 19:35:47 +0100

    exension2 complete

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const getAndRender = () => {
             state.task = data
             removeTasks()
             renderTasks()
-            addCompleteButton()
             // removeCompleteButton()
         })
 }
@@ -53,6 +52,8 @@ const renderTasks = () => {
         taskItem.innerText = task.title
         const completeButton = addCompleteButton(task)
         taskItem.append(completeButton)
+        const deleteButton = addDeleteButton(task)
+        taskItem.append(deleteButton)
         taskList.appendChild(taskItem)
     })
 }
@@ -60,19 +61,43 @@ const renderTasks = () => {
 //function that adds a complete button to each task in the list 
 const addCompleteButton = (task) => {
     const completeButton = document.createElement('button')
+    console.log(task)
     //argument to change innerText on completed task buttons
     if (task.completed) {
         completeButton.innerText = 'Revisit'
     } else {
         completeButton.innerText = 'Complete'
     }
-    console.log(task.completed)
     //event listener to call my completeTask function that changes the completed status of the task to true
     completeButton.addEventListener('click', (event) => {
         completeTask(task.id, !task.completed)
 
     })
     return completeButton
+}
+
+//function to add a delete button to each task in the list
+const addDeleteButton = (task) => {
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = 'Delete'
+    deleteButton.addEventListener('click', (event) => {
+        deleteTask(task.id)
+    })
+    return deleteButton
+}
+
+//function that deletes a task from the list
+const deleteTask = (id) => {
+    const options = {
+        method: 'DELETE'
+    }
+
+    fetch(`${root}/todos/${id}`, options)
+        .then((response) => response.json())
+        .then(data => {
+            console.log("delete task", data)
+            getAndRender()
+        })
 }
 
 //function that changes the completed status of the task to true
