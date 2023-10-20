742cd4279ba1eaf3781985cdf7a882a88fa5d130
Mobeen <muhammadmobeen1@hotmail.com>
2023-10-20 14:44:06 +0200

    original code

--- index.js
+++ index.js
@@ -1,109 +1,69 @@
 const state = {
-    tasks: []
+    todos: []
 }
-const taskUL = document.querySelector("#todo-list")
+
+const toDoList = document.querySelector('#todo-list')
 const form = document.querySelector('form')
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const newTaskTitle = document.querySelector('input').value
-    createNewTask(newTaskTitle)
-})
-const getTasks = () => {
-    fetch("http://localhost:3000/todos")
-        .then((res) => {return res.json()})
-        .then((tasks) => {
-            state.tasks = tasks
-            renderTasks()
-        })
-        .catch((error) => {
-            alert(error)
-        })
-}
-const renderTasks = () => {
-    taskUL.innerHTML = ''
-    state.tasks.forEach((task) => {
-        const li = document.createElement('li')
-
-        const completeBtn = document.createElement('button')
-        completeBtn.innerText = getCompletedBtnText(task.completed)
-        completeBtn.addEventListener('click', () => {
-            updateTaskCompletion(task)
-        })
-        if(task.completed) {
-            li.className = 'completed'
+// Function to fetch todos from the server
+async function fetchTodos() {
+    try {
+        const response = await fetch('http://localhost:3000/todos')
+        if (!response.ok) {
+            throw new Error(`Failed to fetch data: ${response.status}`)
         }
+        const data = await response.json()
+        state.todos = data
+        renderTodoList()
+    } catch (error) {
+        console.error(error)
+    }
+}
 
-        const deleteBtn = document.createElement('button')
-        deleteBtn.innerText = 'Delete'
-        deleteBtn.addEventListener('click', () => {
-            deleteTask(task)
-        })
-
-        li.append(completeBtn)
-        li.append(deleteBtn)
-        li.append(task.title)
-        taskUL.append(li)
+// Function to render the todo list
+function renderTodoList() {
+    toDoList.innerHTML = ''
+    state.todos.forEach(todo => {
+        const listItem = document.createElement('li')
+        listItem.innerText = todo.title
+        toDoList.appendChild(listItem)
     })
 }
-const getCompletedBtnText = (task) => {
-    console.log(task)
-    return task ? "Incomplete" : "Complete"
-}
-const updateTaskCompletion = (task) => {
 
-    const newState = {
-        completed: !task.completed
+// Function to add a new todo
+async function addTodo(newTitle) {
+    try {
+        const newAddition = {
+            title: newTitle,
+            completed: false
+        }
+        const postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newAddition)
+        }
+        const response = await fetch('http://localhost:3000/todos', postOptions)
+        if (!response.ok) {
+            throw new Error(`Failed to add a new todo: ${response.status}`)
+        }
+        const data = await response.json()
+        state.todos.push(data)
+        renderTodoList()
+    } catch (error) {
+        console.error(error)
     }
-
-    const options = {
-        method: "PATCH",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(newState)
-    }
-
-    fetch(`http://localhost:3000/todos/${task.id}`, options)
-        .then(getTasks)
-        .catch((error) => {
-            alert(error)
-        })
-}
-
-const deleteTask = (task) => {
-
-    const options = {
-        method: "DELETE"
-    }
-    
-    fetch(`http://localhost:3000/todos/${task.id}`, options)
-        .then(getTasks)
-        .catch((error) => {
-            alert(error)
-        })
 }
 
-const createNewTask = (newTaskTitle) => {
+// Event listener for form submission
+form.addEventListener('submit', async event => {
+    event.preventDefault()
+    const input = form.querySelector('input')
+    const newTodo = input.value
+    await addTodo(newTodo)
+    form.reset()
+})
 
-    const taskToAdd = {
-        title: newTaskTitle,
-        completed: false
-    }
-
-    const options = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(taskToAdd)
-    }
-
-    fetch("http://localhost:3000/todos", options)
-        .then(getTasks)
-        .catch((error) => {
-            alert(error)
-        })
-}
-
-getTasks()
+// Initial fetch of todos
+fetchTodos();
