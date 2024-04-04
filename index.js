4afe9bdcdd168c976e4e78c6178f35b58eebb772
Angus Townsley <angustownsley@gmail.com>
2024-04-04 16:30:13 +0100

    minor fixes

--- index.js
+++ index.js
@@ -67,7 +67,10 @@ async function newTask(taskTitle) {
             console.log(response)
             render()
         })
-        .catch((err) => console.error(err))
+        .catch((err) => {
+            console.error(err)
+            alert("Task not created, please try again")
+        })
 }
 
 async function deleteTask(id) {
@@ -75,32 +78,44 @@ async function deleteTask(id) {
         method: "DELETE",
     }
 
-    fetch(`https://boolean-api-server.fly.dev/angustownsley/todo/${id}`, options)
+    fetch(
+        `https://boolean-api-server.fly.dev/angustownsley/todo/${id}`,
+        options
+    )
         .then((response) => response.json())
         .then((response) => {
             console.log(response)
             render()
         })
-        .catch((err) => console.error(err))
+        .catch((err) => {
+            console.error(err)
+            alert("Task not deleted, please try again")
+        })
 }
 
 async function completeTask(title, id) {
     const options = {
         method: "PUT",
-        headers: {'Content-Type': 'application/json'},
-        body:JSON.stringify({
-            "title": `${title}`,
-            "completed": true
-        })
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            title: `${title}`,
+            completed: true,
+        }),
     }
 
     console.log(options)
 
-    fetch(`https://boolean-api-server.fly.dev/angustownsley/todo/${id}`, options)
+    fetch(
+        `https://boolean-api-server.fly.dev/angustownsley/todo/${id}`,
+        options
+    )
         .then((response) => response.json())
         .then((response) => {
             console.log(response)
             render()
         })
-        .catch((err) => console.error(err))
+        .catch((err) => {
+            console.error(err)
+            alert("Task not marked as complete, please try again")
+        })
 }
