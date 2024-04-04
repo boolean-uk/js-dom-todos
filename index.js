969c70b54cb2cf4bb4be850b8275aba2d78e12b3
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-04 12:47:28 +0200

    add function to create todo list

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
+const todoList = document.querySelector('#todo-list')
+
 async function getTodo() {
     const response = await fetch('https://boolean-api-server.fly.dev/MyrtheDullaart/todo')
     const data = await response.json()
 
     console.log(data)
+    makeTodoList(data)
+}
+
+function makeTodoList(todoData) {
+    todoList.innerHTML = ''
+    todoData.forEach((item) => {
+        const todoLi = document.createElement('li')
+
+        todoLi.innerText = item.title
+
+        if (item.completed) {
+            todoLi.classList.add('completed')
+        }
+
+        todoList.append(todoLi)
+    });
+    
 }
 
 getTodo()
