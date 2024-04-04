9d5fba50b9e24c9da8da248c06e45e3c0a2de448
Angus Townsley <angustownsley@gmail.com>
2024-04-04 15:47:36 +0100

    complete delete

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const form = document.querySelector("#new-task")
 const toDoList = document.querySelector("#todo-list")
 
-
 async function render() {
     const toDos = await fetch(
         "https://boolean-api-server.fly.dev/angustownsley/todo/"
@@ -21,6 +20,24 @@ function buildList(arr) {
             newItem.style.color = "gray"
         }
 
+        const id = element.id
+        const title = element.title
+
+        const compButton = document.createElement("button")
+        compButton.addEventListener("click", () => {
+            completeTask(title, id)
+        })
+        compButton.innerHTML = "&#x2705"
+
+        const delButton = document.createElement("button")
+        delButton.addEventListener("click", () => {
+            deleteTask(id)
+        })
+        delButton.innerHTML = "&#10060"
+
+        newItem.prepend(delButton)
+        newItem.append(compButton)
+
         toDoList.append(newItem)
     })
 }
@@ -29,26 +46,60 @@ render()
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    taskTitle = e.currentTarget.children[0].children[0].value
+
+    const taskTitle = e.currentTarget.children[0].children[0].value
     console.log(taskTitle)
     newTask(taskTitle)
 })
 
 async function newTask(taskTitle) {
-    
-    
     const options = {
-        method: 'POST',
+        method: "POST",
         body: `{"title":"${taskTitle}"}`,
         headers: {
-            'Content-type': 'application/json',
+            "Content-type": "application/json",
         },
-      }
-    
+    }
+
     fetch("https://boolean-api-server.fly.dev/angustownsley/todo/", options)
         .then((response) => response.json())
         .then((response) => {
-            console.log(response) 
+            console.log(response)
+            render()
+        })
+        .catch((err) => console.error(err))
+}
+
+async function deleteTask(id) {
+    const options = {
+        method: "DELETE",
+    }
+
+    fetch(`https://boolean-api-server.fly.dev/angustownsley/todo/${id}`, options)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
+            render()
+        })
+        .catch((err) => console.error(err))
+}
+
+async function completeTask(title, id) {
+    const options = {
+        method: "PUT",
+        headers: {'Content-Type': 'application/json'},
+        body:JSON.stringify({
+            "title": `${title}`,
+            "completed": true
+        })
+    }
+
+    console.log(options)
+
+    fetch(`https://boolean-api-server.fly.dev/angustownsley/todo/${id}`, options)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response)
             render()
         })
         .catch((err) => console.error(err))
