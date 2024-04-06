24145f1fe029220320f2b0d2fa2446c91c0caaf1
Periklis <perik76@gmail.com>
2024-04-06 03:41:42 +0300

    2nd try done. Refactored according to Nathans code-along

--- index.js
+++ index.js
@@ -1,26 +1,4 @@
-// function createTodoItems(toDo) {
-// 	const toDoLi = document.createElement("li")
-// 	toDoLi.innerText = toDo.title
-
-// 	return toDoLi
-// }
-
-
-// //GET toDos and add them to the list
-// async function getToDos() {
-// 	const toDoData = await fetch("https://boolean-api-server.fly.dev/PerikK/todo")
-// 	const toDos = await toDoData.json()
-// 	console.log(toDos)
-
-// 	const toDoUl = document.getElementById("todo-list")
-
-// 	toDos.forEach((td) => {
-// 		const toDo = createTodoItems(td)
-// 		toDoUl.append(toDo)
-// 	})
-// }
-// getToDos()
-
+//HTML elements
 const inputForm = document.querySelector("form")
 const toDoInput = document.querySelector("input")
 const toDoUl = document.querySelector("#todo-list")
